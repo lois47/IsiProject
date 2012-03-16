72c15a061c39154225e5fec931c949516a7bd81a
Robin <bussenot.robin@gmail.com>
2012-03-16 16:09:36 +0100

    modification du Parser main

--- src/main/java/jaxb/ParserMain.java
+++ src/main/java/jaxb/ParserMain.java
@@ -1,8 +1,12 @@
 package jaxb;
 import java.io.File;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import jaxb_classes.*;
-import javax.xml.bind.*;
+
 
 
 public class ParserMain {
@@ -10,11 +14,13 @@ public class ParserMain {
 	public static void main(String args[]){
 		File fileXML = new File("../../../quiz.xml");
 		try{
-			JAXBContext jc = JAXBContext.newInstance("jaxb");
+			JAXBContext jc = JAXBContext.newInstance("jaxb_classes");
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
 			unmarshaller.setValidating(true);
 			
-			Quiz quiz = (Quiz) unmarshaller.unmarshal(new File("test.xml"));
+			Quiz quiz = (Quiz) unmarshaller.unmarshal(fileXML);
+			List question = quiz.getQuestion();
+			System.out.println(question.get(1).toString());
 		}
 		catch(Exception e) {
 			e.printStackTrace();
